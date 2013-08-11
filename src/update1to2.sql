b08bde8629fe39ddb639b1ec88102de6d382bb6a
Magnus Achim Deininger <magnus@ef.gy>
2013-08-11 17:07:42 +0100

    fix the proxy queries

--- src/update1to2.sql
+++ src/update1to2.sql
@@ -5,3 +5,19 @@ insert or replace into option
     values
     (-1, '2')
 ;
+
+delete
+  from query
+ where flag in ('use-proxy', 'drop-proxy');
+
+insert into query
+    (flag, parameters, query)
+    values
+    ('use-proxy',   1, 'insert or replace into option (otid, value) values (0, ?1)')
+;
+
+insert into query
+    (flag, interval, query)
+    values
+    ('drop-proxy',   null, 'delete from option where otid = 0')
+;
