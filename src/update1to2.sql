d8de9e9c52dfb1c7276b188b908b767fd9eec6d3
Magnus Achim Deininger <magnus@ef.gy>
2013-08-12 10:54:27 +0100

    add the 'update' query to force the daemon to update all queries

--- src/update1to2.sql
+++ src/update1to2.sql
@@ -10,14 +10,15 @@ delete
   from query
  where flag in ('use-proxy', 'drop-proxy');
 
-insert into query
+insert or replace into query
     (flag, parameters, query)
     values
     ('use-proxy',   1, 'insert or replace into option (otid, value) values (0, ?1)')
 ;
 
-insert into query
+insert or replace into query
     (flag, interval, query)
     values
-    ('drop-proxy',   null, 'delete from option where otid = 0')
+    ('drop-proxy',   null, 'delete from option where otid = 0'),
+	('update',       null, 'update feedservice set updated = null')
 ;
