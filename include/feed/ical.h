4fd31b3e9e99e69ab541418ae90217ec4822418b
Magnus Achim Deininger <magnus@ef.gy>
2013-08-17 16:23:16 +0100

    ical parser now working - not doing much with the data but it does get parsed

--- include/feed/ical.h
+++ include/feed/ical.h
@@ -35,6 +35,8 @@
 #include <feed/download.h>
 #include <boost/regex.hpp>
 #include <sstream>
+#include <algorithm>
+#include <boost/tokenizer.hpp>
 
 namespace feed
 {
@@ -63,10 +65,140 @@ namespace feed
             try
             {
                 download::data data = download.retrieve (feed.source);
+                static const boost::regex folded("^\\s(.*)");
+                std::istringstream is(data.content);
+
+                std::vector<std::string> block;
+                std::string line;
+                bool havedata = false;
+
+                for (std::string s; std::getline (is, s); )
+                {
+                    boost::smatch matches;
+
+                    if ((s.size() > 0) && (s[0] != '#'))
+                    {
+                        if (havedata && boost::regex_match(s, matches, folded))
+                        {
+                            line += matches[1];
+                            continue;
+                        }
+
+                        if (havedata)
+                        {
+                            process (block, line);
+                            havedata = false;
+                        }
+
+                        if (!havedata)
+                        {
+                            line = s;
+                            havedata = true;
+                        }
+                    }
+                }
+
+                if (havedata)
+                {
+                    process (block, line);
+                }
             }
             catch (exception &e)
             {
                 std::cerr << "EXCEPTION: " << e.string << "\n";
+            }
+
+            return true;
+        }
+
+        bool process (std::vector<std::string> &block, const std::string &line)
+        {
+            static const boost::regex mime("^\\s*([\\w;=, -]+):\\s*([^\r]*)\r?");
+            static const boost::regex attr("^([^;]*);(.*)$");
+            static const boost::regex attre("^([^=]*)=(.*)$");
+            boost::smatch matches;
+
+            if (!boost::regex_match(line, matches, mime))
+            {
+                return false;
+            }
+
+            std::string key    = matches[1];
+            std::string value  = matches[2];
+            std::string keyl   = key;
+            std::string valuel = value;
+            std::transform(keyl.begin(),   keyl.end(),   keyl.begin(),   (int(*)(int))std::tolower);
+            std::transform(valuel.begin(), valuel.end(), valuel.begin(), (int(*)(int))std::tolower);
+            std::string cblock = "";
+            std::map<std::string,std::vector<std::string> > attributes;
+            std::string attribute = keyl;
+            bool haveattribute = false;
+            if (block.size() > 0)
+            {
+                cblock = block.back();
+            }
+
+            for (bool first = true; boost::regex_match(attribute, matches, attr); first = false)
+            {
+                if (first)
+                {
+                    keyl = matches[1];
+                }
+                else if (haveattribute)
+                {
+                    boost::smatch q;
+                    if (boost::regex_match(std::string(matches[1]), q, attre))
+                    {
+                        std::string av(q[2]);
+                        boost::char_separator<char> sep(",");
+                        boost::tokenizer<boost::char_separator<char> > tok(av, sep);
+                        std::copy(tok.begin(), tok.end(), std::back_inserter(attributes[q[1]]));
+                    }
+                }
+
+                attribute = matches[2];
+                haveattribute = true;
+            }
+
+            if (haveattribute)
+            {
+                boost::smatch q;
+                if (boost::regex_match(attribute, q, attre))
+                {
+                    std::string av(q[2]);
+                    boost::char_separator<char> sep(",");
+                    boost::tokenizer<boost::char_separator<char> > tok(av, sep);
+                    std::copy(tok.begin(), tok.end(), std::back_inserter(attributes[q[1]]));
+                }
+            }
+
+            if (keyl == "begin")
+            {
+                block.push_back(valuel);
+            }
+            else if ((block.size() > 0) && (keyl == "end"))
+            {
+                block.pop_back();
+            }
+            else
+            {
+                std::cerr << cblock << "::" << keyl << "(" << attributes.size() << ")" << "=" << value << "\n";
+                if (attributes.size() > 0)
+                {
+                    for (std::map<std::string,std::vector<std::string> >::iterator it = attributes.begin();
+                         it != attributes.end();
+                         it++)
+                    {
+                        std::cerr << "[" << it->first << "] = ";
+                        for (std::vector<std::string>::iterator it2 = it->second.begin();
+                             it2 != it->second.end();
+                             it2++)
+                        {
+                            std::cerr << " . " << *it2;
+                        }
+                        std::cerr << "\n";
+                    }
+                } 
             }
 
             return true;
